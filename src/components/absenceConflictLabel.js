8c5ce9024e62af90e7935d89718af45754a57a54
xalexkm <xalexkm@gmail.com>
2024-06-21 00:33:25 +0100

    fix: clean up errors

--- src/components/absenceConflictLabel.js
+++ src/components/absenceConflictLabel.js
@@ -4,12 +4,12 @@ import {memo} from "react";
 
 
 const AbsenceConflictLabel = ({ employeeId }) => {
-    const { data, isLoading} = useQuery({
+    const { data, isLoading, isError} = useQuery({
         queryKey: ["absence-conflict-label", employeeId],
         queryFn: () => getAbsenceConflict(employeeId)
     })
 
-    return <>{ (!isLoading && data && data.conflicts) && <div className="p-1 text-sm rounded-md bg-red-500 text-white">Conflicts</div>}</>
+    return <>{ (!isLoading && !isError && data && data.conflicts) && <div className="p-1 text-sm rounded-md bg-red-500 text-white">Conflicts</div>}</>
 }
 
 export default memo(AbsenceConflictLabel)
