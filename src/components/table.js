6d78d0d0d42e675b01c5e9908a8efd41e77311a1
xalexkm <xalexkm@gmail.com>
2024-06-20 23:10:38 +0100

    fix: set correct indicator when column is sorted

--- src/components/table.js
+++ src/components/table.js
@@ -5,6 +5,7 @@ import {IoArrowDownOutline} from "react-icons/io5";
 export default function Table({ data }) {
     const [view, setView] = useState(data);
     const [isFiltered, setIsFiltered] = useState(false);
+    const [sortedColumn, setSortedColumn] = useState(null)
 
     const onNameClick = (id) => {
         const filteredView = data.filter((employee) => employee.employee.id === id)
@@ -18,6 +19,7 @@ export default function Table({ data }) {
     }
 
     const handleSort = (sortingProp) => {
+        setSortedColumn(sortingProp);
         const sortedView = [...view].sort((a, b) => {
             let aVal
             let bVal
@@ -52,11 +54,11 @@ export default function Table({ data }) {
             <table className="table">
                 <thead className="table-header-group">
                 <tr className="table-row">
-                    <th className={'table-cell'}><div onClick={() => handleSort('name')} className="flex items-center">Name <IoArrowDownOutline/></div></th>
-                    <th className={'table-cell'}><div onClick={() => handleSort('absenceType')} className="flex gap-2 justify-center items-center">Absence Type <IoArrowDownOutline/></div></th>
+                    <th className={'table-cell'}><div onClick={() => handleSort('name')} className="flex justify-center items-center">Name { sortedColumn === 'name' && <IoArrowDownOutline/>}</div></th>
+                    <th className={'table-cell'}><div onClick={() => handleSort('absenceType')} className="flex gap-2 justify-center items-center">Absence Type { sortedColumn === 'absenceType' && <IoArrowDownOutline/>}</div></th>
                     <th className={'table-cell text-xs max-w-32'}>Approved/Pending Approval</th>
-                    <th className={'table-cell'}><div onClick={() => handleSort('startDate')} className="flex gap-2 justify-center items-center">Start Date <IoArrowDownOutline/></div></th>
-                    <th className={'table-cell'}><div onClick={() => handleSort('endDate')} className="flex gap-2 justify-center items-center">End Date <IoArrowDownOutline/></div></th>
+                    <th className={'table-cell'}><div onClick={() => handleSort('startDate')} className="flex gap-2 justify-center items-center">Start Date { sortedColumn === 'startDate' && <IoArrowDownOutline/>}</div></th>
+                    <th className={'table-cell'}><div onClick={() => handleSort('endDate')} className="flex gap-2 justify-center items-center">End Date { sortedColumn === 'endDate' && <IoArrowDownOutline/>}</div></th>
                 </tr>
                 </thead>
                 <tbody className="table-row-group">
